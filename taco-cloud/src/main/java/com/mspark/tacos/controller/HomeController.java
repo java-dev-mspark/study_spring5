b56efd9ed80bfc591f0e8137d4466f8ecccddd67
mspark <altntaos@gmail.com>
2020-10-07 11:03:15 +0900

    Chapter1 commit

---
+++ taco-cloud/src/main/java/com/mspark/tacos/controller/HomeController.java
@@ -0,0 +1,13 @@
+package com.mspark.tacos.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class HomeController {
+
+	@GetMapping("/")
+	public String home() {
+		return "home";
+	}
+}
